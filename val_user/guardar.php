550e07c78a5122e20e3311300d396e3354b9c10d
stephentapia <stephentapia21@gmail.com>
2023-06-26 19:27:01 -0400

    asdasd

--- val_user/guardar.php
+++ val_user/guardar.php
@@ -19,9 +19,12 @@ if (mysqli_num_rows($resultado)>0)
     window.location.href="../login/registro.php";</script>';
  
 } else {
+
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE id = ?");
+    mysqli_stmt_bind_param($stmt, 'i', $id);
 	
 	$insert_value = "INSERT INTO login (email, nombre, apellido, pass) VALUES ('$ano', '$mes', '$dia ','$pais')";
- 
+    mysqli_stmt_execute($stmt);
     $retry_value = mysqli_query($conn, $insert_value);
  
     if (!$retry_value) {
