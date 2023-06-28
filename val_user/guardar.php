8b5f6ae7604e69c52e0c535dbf2f40a9f04d0bca
stephentapia <stephentapia21@gmail.com>
2023-06-28 17:57:37 -0400

    asdad

--- val_user/guardar.php
+++ val_user/guardar.php
@@ -1,43 +1,37 @@
 <?php
 
-include '../conexion/conn.php';
+require '../conexion/conn.php';
 
 $email= $_POST['email'];
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $pass = $_POST['pass'];
 
-
-$stmt1 = mysqli_prepare($conn, "SELECT * FROM login WHERE email= ?");
+$stmt1 = mysqli_prepare($conn, "SELECT * FROM Login WHERE email= ?");
     
 mysqli_stmt_bind_param($stmt1, 's', $email);    
 
-$insert_value1 = mysqli_stmt_execute($stmt1);
+mysqli_stmt_execute($stmt1);
+var_dump("por aca paso");
+var_dump($insert_value1);
+var_dump("por aca paso");
+$result = mysqli_stmt_get_result($stmt1);
 
-$resultado = mysqli_query($conn, $insert_value1);
- 
-if (mysqli_num_rows($resultado)>0)
+if (mysqli_num_rows($result) > 0)
 {
  
     echo'<script type="text/javascript"> alert("Este correo ya esta ingresado");
-    window.location.href="../login/registro.php";</script>';
+    window.location.href="../login/registro.html";</script>';
  
 } else {
 
     $stmt = mysqli_prepare($conn, "INSERT INTO Login (email, nombre, apellido, pass) VALUES (?, ?, ?, ?)");
     
-    mysqli_stmt_bind_param($stmt, 'ssss', $email, $nombre, $apellido, $pass);    
+    $stmt->bind_param("ssss", $email, $nombre, $apellido, $pass);    
 
-	$insert_value = mysqli_stmt_execute($stmt);
+	$stmt->execute();
 
-    $retry_value = mysqli_query($conn, $insert_value);
- 
-    if (!$retry_value) {
-        echo'<script type="text/javascript"> alert("Fallo el ingresado, porfavor, intentalo nuevamente");
-        window.location.href="../login/registro.html";</script>';
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
-    }
+    
 	
     echo'<script type="text/javascript"> alert("¡¡¡ Ingreso exitoso !!!");
     window.location.href="../login/index.html";</script>';
